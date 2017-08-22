1e7bdeced9e2f26bdd514ef66645ce03ae11a8a8
GabrielScalici <gabrielhc.scalici@gmail.com>
2017-08-22 11:16:00 -0300

    Primeiro exercicio de python

--- ex01.py
+++ ex01.py
@@ -1,2 +1,13 @@
-a = input('Digite um numero')
-print(a)
+print('1 - Fahrenheit para Celsius')
+print('2 - Celsius para Fharenheit')
+
+op = input();
+
+if op == 1:
+    far = input('Digite o numero em Fahrennheit\n')
+    total = (5 * far)/9 - 32
+else:
+    cel = input('Digite o numero em Celsius\n')
+    total = (9 * cel)/5 + 32
+
+print 'Conversao:',total
